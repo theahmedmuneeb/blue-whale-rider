baefe5852bb7ea348f924fabce0f0c0d6a4c654e
theahmedmuneeb <101744347+theahmedmuneeb@users.noreply.github.com>
2022-05-06 23:04:58 +0500

    Add files via upload

---
+++ seo-and-responsive-goggle-script.js
@@ -0,0 +1,130 @@
+﻿ function pageReload(){var n=document.querySelector('#copyrights-pop .popBo .popBtn svg');null!=n&&n.classList.add('r'),setTimeout(function(){window.location.reload()},500)};
+
+    window.onload = function () {
+
+      var i = 'https://techwithmuneeb.blogspot.com';
+
+      var w = 'http://techwithmuneeb.blogspot.com';
+
+      var a = window.location.hostname;
+
+      var e = document.getElementById('license');
+
+      second = 10;
+
+  	var o = document.getElementById('license').innerText;
+
+      var s = document.createElement('div');
+
+      s.id = 'cpr';
+
+  	s.innerHTML = '
+<div id="copyrights-pop" class="popSc">
+<div class="popBo">
+<div class="icn error">
+<svg class="icn error" width="50px" height="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve">
+<g xmlns="http://www.w3.org/2000/svg">
+<path d="M240.625 467.492h-.003c-66.386 0-122.241-3.115-165.759-6.932-26.825-2.353-43.621-30.123-33.337-55.01 28.526-69.035 63.745-137.231 105.061-203.371 28.123-45.021 55.272-82.968 78.23-112.824 16.011-20.822 47.334-20.828 63.346-.007 22.844 29.707 49.848 67.439 77.831 112.183 41.105 65.725 76.192 133.492 104.671 202.102 10.34 24.909-6.521 52.72-33.387 55.081-66.342 5.831-132.272 8.778-196.653 8.778z" fill="#dd636e"/>
+<path d="M265.871 74.846c-7.79 2.048-15.082 6.933-20.474 14.644-21.061 30.12-45.964 68.407-71.76 113.828-37.902 66.727-70.211 135.528-96.374 205.169-8.552 22.75 3.3 47.889 23.914 54.174a1747.882 1747.882 0 0 1-26.313-2.101c-26.829-2.355-43.618-30.12-33.341-55.015C70.05 336.518 105.274 268.321 146.59 202.18c28.124-45.018 55.269-82.973 78.229-112.821 10.135-13.183 26.417-18.023 41.052-14.513z" fill="#da4a54"/>
+<path d="M441.838 272.061v-2.894c0-10.208 7.904-18.673 18.089-19.372l31.327-2.149c11.221-.77 20.746 8.125 20.746 19.372v7.192c0 11.247-9.526 20.142-20.746 19.372l-31.327-2.149c-10.184-.698-18.089-9.163-18.089-19.372zm-24.174-93.113-1.447-2.507c-5.104-8.841-2.491-20.124 5.979-25.821l26.055-17.524c9.333-6.277 22.029-3.337 27.653 6.403l3.596 6.229c5.624 9.74 1.821 22.206-8.281 27.15l-28.204 13.802c-9.169 4.488-20.247 1.109-25.351-7.732zm-67.492-68.551-2.507-1.447c-8.841-5.104-12.219-16.182-7.732-25.351l13.802-28.204c4.944-10.102 17.41-13.904 27.15-8.281l6.229 3.596c9.74 5.624 12.68 18.32 6.403 27.653l-17.524 26.055c-5.697 8.47-16.98 11.083-25.821 5.979zM70.162 272.061v-2.894c0-10.208-7.904-18.673-18.089-19.372l-31.327-2.149C9.526 246.876 0 255.771 0 267.018v7.192c0 11.247 9.526 20.142 20.746 19.372l31.327-2.149c10.184-.698 18.089-9.163 18.089-19.372zm24.174-93.113 1.447-2.507c5.104-8.841 2.491-20.124-5.979-25.821l-26.055-17.524c-9.333-6.277-22.029-3.337-27.653 6.403l-3.596 6.229c-5.624 9.74-1.821 22.206 8.281 27.15l28.204 13.802c9.169 4.488 20.247 1.109 25.351-7.732zm67.492-68.551 2.507-1.447c8.841-5.104 12.219-16.182 7.732-25.351l-13.802-28.204c-4.944-10.102-17.41-13.904-27.15-8.281l-6.229 3.596c-9.74 5.624-12.68 18.32-6.403 27.653l17.524 26.055c5.697 8.47 16.98 11.083 25.821 5.979z" fill="#dd636e"/>
+<path d="M244.901 180.12c7.47-.751 14.94-.751 22.41 0 9.451.951 16.686 8.844 17.035 18.336a1270.015 1270.015 0 0 1 0 93.147c-.348 9.492-7.584 17.386-17.035 18.336-7.47.751-14.94.751-22.41 0-9.451-.951-16.686-8.844-17.035-18.336a1270.015 1270.015 0 0 1 0-93.147c.349-9.493 7.584-17.386 17.035-18.336zm.542 163.895c7.109-.34 14.218-.34 21.327 0 9.797.469 17.665 8.355 18.107 18.153.431 9.552.431 19.104 0 28.657-.442 9.798-8.31 17.684-18.107 18.153-7.109.34-14.218.34-21.327 0-9.797-.469-17.665-8.355-18.107-18.153a317.727 317.727 0 0 1 0-28.657c.442-9.798 8.31-17.684 18.107-18.153z" fill="#fff"/>
+</g>
+</svg>
+</div>
+<h2>Copied Theme!
+</h2>
+<p>Looks like the owner of the site copied this theme  from 
+<a href="https://techwithmuneeb.blogspot.com">Tech With Muneeb
+</a> if you are the owner and already have license from us then contact the owner. 
+<br />
+<span href="https://techwithmuneeb.blogspot.com/p/contact.html" class="button">Contact Now
+</span>
+</p>
+<button class="popBtn" onclick="pageReload()">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+<polyline points="23 4 23 10 17 10" />
+<path d="M20.49 15a9 9 0 1 1-2.12-9.36L23 10" />
+</svg>
+</button>
+</div>
+</div>
+<style>.popSc{position:fixed;top:0;bottom:0;left:0;right:0;padding:20px;background:rgba(255,255,255,.1);z-index:99980;-webkit-backdrop-filter:blur(10px);backdrop-filter:blur(10px);display:flex;justify-content:center;align-items:center}.popSc.hidden{display:none}.popSc .popBo{position:relative;background:rgba(255,255,255,.8);-webkit-backdrop-filter:blur(10px);backdrop-filter:blur(10px);max-width:400px;display:flex;justify-content:center;align-items:center;flex-direction:column;padding:30px;border-radius:20px;box-shadow:0 5px 25px rgb(0 0 0 / 20%)}.popSc .popBo .icn{width:50px!important;height:50px!important}.popSc .popBo h2{margin:10px 0 15px 0;font-size:1.2rem;font-weight:800;color:#08102b}.popSc .popBo p{margin:0;line-height:1.7em;font-size:.9rem;color:#08102b}.popSc .popBo .popBtn{display:inline-flex;justify-content:center;align-items:center;height:50px;width:50px;outline:0;border:none;background:#482dff;border-radius:50%;margin-top:20px;transition:all .2s ease;-webkit-transition:all .2s ease}.popSc .popBo .popBtn:hover{transform:scale(1.05);-webkit-transform:scale(1.05)}.popSc .popBo .popBtn svg{width:24px;height:24px;stroke:#fff;flex-shrink:0}.popSc .popBo .popBtn svg.r{animation:rotateIcn 1.5s infinite linear;-webkit-animation:rotateIcn 1.5s infinite linear}.popSc{animation:popupBlur .3s ease-in;-webkit-animation:popupBlur .3s}.popSc>*{animation:popupScale .3s ease-in;-webkit-animation:popupScale .3s}.drkM .popSc{background:rgba(0,0,0,.1)}.drkM .popSc .popBo{background:rgba(50,50,50,.8)}.drkM .popSc .popBo h2,.drkM .popSc .popBo p{color:#fefefe}@keyframes popupBlur{from{opacity:0}to{opacity:1}}@-webkit-keyframes popupBlur{from{opacity:0}to{opacity:1}}@keyframes popupScale{from{transform:scale(0);animation-timing-function:ease-in;opacity:0}to{transform:scale(1);opacity:1}}@-webkit-keyframes popupScale{from{-webkit-transform:scale(0);-webkit-animation-timing-function:ease-in;opacity:0}to{-webkit-transform:scale(1);opacity:1}}@keyframes rotateIcn{from{transform:rotate(0)}to{transform:rotate(359deg)}}@-webkit-keyframes rotateIcn{from{-webkit-transform:rotate(0)}to{-webkit-transform:rotate(359deg)}}#license{display:none}
+</style>';
+
+      if (e === undefined) {
+
+        document.body.appendChild(s);
+
+      }
+
+      else if (e === null || a !== w || document.getElementById('license').innerText !== 'FF571D@4897386763424370153') {
+
+        s.innerHTML = '
+<div id="copyrights-pop" class="popSc">
+<div class="popBo">
+<div class="icn error">
+<svg class="icn error" width="50px" height="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve">
+<g xmlns="http://www.w3.org/2000/svg">
+<path d="M240.625 467.492h-.003c-66.386 0-122.241-3.115-165.759-6.932-26.825-2.353-43.621-30.123-33.337-55.01 28.526-69.035 63.745-137.231 105.061-203.371 28.123-45.021 55.272-82.968 78.23-112.824 16.011-20.822 47.334-20.828 63.346-.007 22.844 29.707 49.848 67.439 77.831 112.183 41.105 65.725 76.192 133.492 104.671 202.102 10.34 24.909-6.521 52.72-33.387 55.081-66.342 5.831-132.272 8.778-196.653 8.778z" fill="#dd636e"/>
+<path d="M265.871 74.846c-7.79 2.048-15.082 6.933-20.474 14.644-21.061 30.12-45.964 68.407-71.76 113.828-37.902 66.727-70.211 135.528-96.374 205.169-8.552 22.75 3.3 47.889 23.914 54.174a1747.882 1747.882 0 0 1-26.313-2.101c-26.829-2.355-43.618-30.12-33.341-55.015C70.05 336.518 105.274 268.321 146.59 202.18c28.124-45.018 55.269-82.973 78.229-112.821 10.135-13.183 26.417-18.023 41.052-14.513z" fill="#da4a54"/>
+<path d="M441.838 272.061v-2.894c0-10.208 7.904-18.673 18.089-19.372l31.327-2.149c11.221-.77 20.746 8.125 20.746 19.372v7.192c0 11.247-9.526 20.142-20.746 19.372l-31.327-2.149c-10.184-.698-18.089-9.163-18.089-19.372zm-24.174-93.113-1.447-2.507c-5.104-8.841-2.491-20.124 5.979-25.821l26.055-17.524c9.333-6.277 22.029-3.337 27.653 6.403l3.596 6.229c5.624 9.74 1.821 22.206-8.281 27.15l-28.204 13.802c-9.169 4.488-20.247 1.109-25.351-7.732zm-67.492-68.551-2.507-1.447c-8.841-5.104-12.219-16.182-7.732-25.351l13.802-28.204c4.944-10.102 17.41-13.904 27.15-8.281l6.229 3.596c9.74 5.624 12.68 18.32 6.403 27.653l-17.524 26.055c-5.697 8.47-16.98 11.083-25.821 5.979zM70.162 272.061v-2.894c0-10.208-7.904-18.673-18.089-19.372l-31.327-2.149C9.526 246.876 0 255.771 0 267.018v7.192c0 11.247 9.526 20.142 20.746 19.372l31.327-2.149c10.184-.698 18.089-9.163 18.089-19.372zm24.174-93.113 1.447-2.507c5.104-8.841 2.491-20.124-5.979-25.821l-26.055-17.524c-9.333-6.277-22.029-3.337-27.653 6.403l-3.596 6.229c-5.624 9.74-1.821 22.206 8.281 27.15l28.204 13.802c9.169 4.488 20.247 1.109 25.351-7.732zm67.492-68.551 2.507-1.447c8.841-5.104 12.219-16.182 7.732-25.351l-13.802-28.204c-4.944-10.102-17.41-13.904-27.15-8.281l-6.229 3.596c-9.74 5.624-12.68 18.32-6.403 27.653l17.524 26.055c5.697 8.47 16.98 11.083 25.821 5.979z" fill="#dd636e"/>
+<path d="M244.901 180.12c7.47-.751 14.94-.751 22.41 0 9.451.951 16.686 8.844 17.035 18.336a1270.015 1270.015 0 0 1 0 93.147c-.348 9.492-7.584 17.386-17.035 18.336-7.47.751-14.94.751-22.41 0-9.451-.951-16.686-8.844-17.035-18.336a1270.015 1270.015 0 0 1 0-93.147c.349-9.493 7.584-17.386 17.035-18.336zm.542 163.895c7.109-.34 14.218-.34 21.327 0 9.797.469 17.665 8.355 18.107 18.153.431 9.552.431 19.104 0 28.657-.442 9.798-8.31 17.684-18.107 18.153-7.109.34-14.218.34-21.327 0-9.797-.469-17.665-8.355-18.107-18.153a317.727 317.727 0 0 1 0-28.657c.442-9.798 8.31-17.684 18.107-18.153z" fill="#fff"/>
+</g>
+</svg>
+</div>
+<h2>Copied Theme!
+</h2>
+<p>Looks like the owner of the site copied this theme from 
+<a href="https://techwithmuneeb.blogspot.com">Tech With Muneeb
+</a> if you are the owner and already have license from us then contact us now. 
+<br />
+<span href="https://techwithmuneeb.blogspot.com/p/contact.html" class="button">Contact Now
+</span>
+</p>
+<div id="cntDwn" style = "display:none">You will be redirected after 
+<span id = "cntd">
+</span>&nbsp;seconds.
+</div>
+</div>
+</div>
+<style>.popSc{position:fixed;top:0;bottom:0;left:0;right:0;padding:20px;background:rgba(255,255,255,.1);z-index:99980;-webkit-backdrop-filter:blur(10px);backdrop-filter:blur(10px);display:flex;justify-content:center;align-items:center}.popSc.hidden{display:none}.popSc .popBo{position:relative;background:rgba(255,255,255,.8);-webkit-backdrop-filter:blur(10px);backdrop-filter:blur(10px);max-width:400px;display:flex;justify-content:center;align-items:center;flex-direction:column;padding:30px;border-radius:20px;box-shadow:0 5px 25px rgb(0 0 0 / 20%)}.popSc .popBo .icn{width:50px!important;height:50px!important}.popSc .popBo h2{margin:10px 0 15px 0;font-size:1.2rem;font-weight:800;color:#08102b}.popSc .popBo p{margin:0;line-height:1.7em;font-size:.9rem;color:#08102b}.popSc .popBo .popBtn{display:inline-flex;justify-content:center;align-items:center;height:50px;width:50px;outline:0;border:none;background:#482dff;border-radius:50%;margin-top:20px;transition:all .2s ease;-webkit-transition:all .2s ease}.popSc .popBo .popBtn:hover{transform:scale(1.05);-webkit-transform:scale(1.05)}.popSc .popBo .popBtn svg{width:24px;height:24px;stroke:#fff;flex-shrink:0}.popSc .popBo .popBtn svg.r{animation:rotateIcn 1.5s infinite linear;-webkit-animation:rotateIcn 1.5s infinite linear}.popSc{animation:popupBlur .3s ease-in;-webkit-animation:popupBlur .3s}.popSc>*{animation:popupScale .3s ease-in;-webkit-animation:popupScale .3s}.drkM .popSc{background:rgba(0,0,0,.1)}.drkM .popSc .popBo{background:rgba(50,50,50,.8)}.drkM .popSc .popBo h2,.drkM .popSc .popBo p{color:#fefefe}@keyframes popupBlur{from{opacity:0}to{opacity:1}}@-webkit-keyframes popupBlur{from{opacity:0}to{opacity:1}}@keyframes popupScale{from{transform:scale(0);animation-timing-function:ease-in;opacity:0}to{transform:scale(1);opacity:1}}@-webkit-keyframes popupScale{from{-webkit-transform:scale(0);-webkit-animation-timing-function:ease-in;opacity:0}to{-webkit-transform:scale(1);opacity:1}}@keyframes rotateIcn{from{transform:rotate(0)}to{transform:rotate(359deg)}}@-webkit-keyframes rotateIcn{from{-webkit-transform:rotate(0)}to{-webkit-transform:rotate(359deg)}}#license{display:none}
+</style>';
+
+        function DelayRedirect() {
+
+    var seconds = 5;
+
+    var cntDwn = document.getElementById("cntDwn");
+
+    var cntd = document.getElementById("cntd");
+
+    cntDwn.style.display = "block";
+
+    cntd.innerHTML = seconds;
+
+    setInterval(function () {
+
+        seconds--;
+
+        cntd.innerHTML = seconds;
+
+        if (seconds == 0) {
+
+            cntDwn.style.display = "none";
+
+            window.location = i;
+
+        }
+
+    }, 1000);
+
+};
+
+        document.body.appendChild(s);
+
+        DelayRedirect()
+
+      }
+
+    }
+
